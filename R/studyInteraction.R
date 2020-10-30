c85b66e927c03fa5e588537e0825b9dca8a8284d
ben-domingue <ben.domingue@gmail.com>
2020-10-30 12:41:20 -0700

    xz option

--- R/studyInteraction.R
+++ R/studyInteraction.R
@@ -2,13 +2,19 @@ studyInteraction<-function(dummy.index=NA,
                            b0=0,b1,b2,b3=0,rho=0.3,N=1000,
                            gammaToY,
                            model,
+                           xz=NULL,
                            sim_xz=NULL,
                            transformY=NULL,
                            ...) {
-    if (is.null(sim_xz)) {
-        library(MASS)
-        xz<-mvrnorm(N,mu=c(0,0),Sigma=matrix(c(1,rho,rho,1),2,2))
-    } else xz<-sim_xz(N,rho)
+    if (is.null(xz)) {
+        if (is.null(sim_xz)) {
+            library(MASS)
+            xz<-mvrnorm(N,mu=c(0,0),Sigma=matrix(c(1,rho,rho,1),2,2))
+        } else xz<-sim_xz(N,rho)
+    } else {
+        index<-sample(1:nrow(xz),nrow(xz),replace=TRUE)
+        xz<-xz[index,]
+    }
     x<-xz[,1]
     z<-xz[,2]
     gamma<-b0+b1*x+b2*z+b3*x*z
--- README.md
+++ README.md
@@ -4,5 +4,4 @@
 
 This R package is meant to conduct analysis to guide statistical inference (via information about FDR and power) in study of interactions.
 
-See more info at: See https://github.com/ben-domingue/InteractionStudio/wiki/Home/_edit
-
+See more info at: https://github.com/ben-domingue/InteractionStudio/wiki
