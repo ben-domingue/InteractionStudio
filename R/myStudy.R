18487a19352766bf6593650abc7cd8b78fec2024
ben-domingue <ben.domingue@gmail.com>
2020-10-30 13:29:13 -0700

    tweaks

--- R/myStudy.R
+++ R/myStudy.R
@@ -20,23 +20,3 @@ myStudy<-function(xz,y=NULL,b3seq,model.base,mc.cores=1,Nsim=100,...) {
 }
 
 
-b1<-1
-b2<-1
-x<-rnorm(100)
-z<-runif(100)
-xz<-cbind(x,z)
-##note no rho or N here
-library(InteractionStudio)
-gammaToY<-gammaToY_options("linear")
-model.base<-model_options("linear",print=TRUE)
-model<-model_options("linear",print=FALSE)
-y<-b1*x+b2*z+rnorm(length(x))
-
-p<-myStudy(xz=xz,y=y,
-           b3seq=seq(0,.5,by=.05),
-           Nsim=300,
-           gammaToY=gammaToY,
-           model.base=model.base,
-           model=model
-           )	    
-plot(p,type='b')
